e9697086d3a919be7f368d16d713b9af7555049e
Harper Nettesheim <harpernettesheim@gmail.com>
2020-08-31 17:43:17 -0500

    Almost there

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,36 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+# x.append(4)
+# print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+# x.append(y[0])
+# x.append(y[1])
+# x.append(y[2])
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+# x.append(4)
+# x.append(y[1])
+# x.append(y[2])
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.append(4)
+x.append(y[1])
+x.append(y[1] * 11)
+x.append(y[2])
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for num in x:
+    print(num * 1000)
